0eb15d3d727d6acf3ed03f5d02e2661cfc5d6c72
Luke Leiby <lucaswleiby@gmail.com>
2022-05-02 15:34:15 -0400

    Introduce cargo workflow

---
+++ hello_cargo/target/debug/deps/hello_cargo-c5ee3f8973577d48.d
@@ -0,0 +1,5 @@
+C:\Users\Luke\Dropbox\Programming\Projects\rust_tutorial\hello_cargo\target\debug\deps\hello_cargo-c5ee3f8973577d48.rmeta: src\main.rs
+
+C:\Users\Luke\Dropbox\Programming\Projects\rust_tutorial\hello_cargo\target\debug\deps\hello_cargo-c5ee3f8973577d48.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Luke\Dropbox\Programming\Projects\rust_tutorial\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Luke\Dropbox\Programming\Projects\rust_tutorial\hello_cargo\src\main.rs
