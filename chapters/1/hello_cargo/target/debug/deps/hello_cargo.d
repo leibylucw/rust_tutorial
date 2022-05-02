a476a1f93cf5c74a2046011dd05662919abc7a0f
Luke Leiby <lucaswleiby@gmail.com>
2022-05-02 19:55:22 -0400

    Remove leftover target directory

--- chapters/1/hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Luke\Dropbox\Programming\Projects\rust_tutorial\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-C:\Users\Luke\Dropbox\Programming\Projects\rust_tutorial\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
--- chapters/1/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Luke\Dropbox\Programming\Projects\rust_tutorial\chapter_1\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Luke\Dropbox\Programming\Projects\rust_tutorial\chapter_1\hello_cargo\src\main.rs
