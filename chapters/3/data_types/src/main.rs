830507da9a5c60d5ee80d741831342c33becac18
Luke Leiby <lucaswleiby@gmail.com>
2022-05-07 16:22:32 -0400

    Test basic data types

---
+++ chapters/3/data_types/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let x: i32 = -2;
+    println!("The value of x is: {}", x);
+
+    let y: f64 = 333.1726345;
+    println!("The value of y is: {}", y);
+
+    let z: bool = true;
+    println!("The value of z is: {}", z);
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("The value of a[0] is: {}", a[0]);
+}
